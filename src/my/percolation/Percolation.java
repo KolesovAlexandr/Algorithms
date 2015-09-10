af5944cbf2865f3079b4eac086b5c218311b0365
Aleksandr Kolesov <aleksandr_kolesov@epam.com>
2015-09-10 16:58:21 +0300

    percolation implemented

--- src/my/percolation/Percolation.java
+++ src/my/percolation/Percolation.java
@@ -11,61 +11,68 @@ public class Percolation {
     UnionFind unionFind;
     boolean[] isOpen;
     Random random;
+    double count;
 
     public Percolation(int N) {
-        unionFind = new UnionFind(N*N+2);
-        isOpen = new boolean[N*N];
+        unionFind = new UnionFind(N * N + 2);
+        isOpen = new boolean[N * N];
         size = N;
-        random = new Random(N*N);
+        random = new Random(N * N);
+        count = 0;
     }
 
-    public void main(String[] args){
+    static public void main(String[] args) {
 
-        Percolation percolation = new Percolation(100);
+        Percolation percolation = new Percolation(10000);
+//        double count = 0;
 
+        while (!percolation.isPercolation()) {
+            percolation.randOpen();
 
-
-
-
-
-    }
-    public void randOpen(){
-        open(random.nextInt(size*size));
-    }
-
-    public void open(int i){
-        isOpen[i]= true;
-        int qSize =size*size;
-
-        if (i-1>0 && isOpen[i-1]) {
-            unionFind.union(i,i-1);
         }
-        if (i+1 < size*size-2 && isOpen[i+1]){
-            unionFind.union(i,i+1);
-        }
-        if(i-size>0 && isOpen[i-size]){
-            unionFind.union(i,i-size);
-        }
-        if (i+size<size*size && isOpen[i+size]){
-            unionFind.union(i,i+size);
-        }
-
-        if( i>=0 & i<size ){
-            unionFind.union(i,qSize);
-        }
-        if (i >= qSize-size & i<qSize) {
-            unionFind.union(i,qSize+1);
-        }
+        double p = percolation.count / (percolation.size * percolation.size);
+        System.out.println(p);
 
     }
 
-    public boolean isPercolation(){
-        int qSize =size*size;
-        return unionFind.conected(qSize,qSize+1);
+    public void randOpen() {
+        open(random.nextInt(size * size));
     }
 
+    public void open(int i) {
+        if (isOpen[i] == true) {
+            return;
+        }
+        count++;
+        isOpen[i] = true;
+        int qSize = size * size;
 
+        if (i - 1 > 0 && isOpen[i - 1]) {
+            unionFind.union(i, i - 1);
+        }
+        if (i + 1 < size * size - 2 && isOpen[i + 1]) {
+            unionFind.union(i, i + 1);
+        }
+        if (i - size > 0 && isOpen[i - size]) {
+            unionFind.union(i, i - size);
+        }
+        if (i + size < size * size && isOpen[i + size]) {
+            unionFind.union(i, i + size);
+        }
 
+        if (i >= 0 & i < size) {
+            unionFind.union(i, qSize);
+        }
+        if (i >= qSize - size & i < qSize) {
+            unionFind.union(i, qSize + 1);
+        }
+
+    }
+
+    public boolean isPercolation() {
+        int qSize = size * size;
+        return unionFind.conected(qSize, qSize + 1);
+    }
 
 
 }
