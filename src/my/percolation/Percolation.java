f51a0be8a5cabd7368ca4d26a98412d07329e85e
Aleksandr Kolesov <aleksandr_kolesov@epam.com>
2015-09-10 16:11:52 +0300

    added methods to Classes

--- src/my/percolation/Percolation.java
+++ src/my/percolation/Percolation.java
@@ -1,7 +1,71 @@
 package my.percolation;
 
+import java.util.Random;
+
 /**
  * Created by Aleksandr_Kolesov on 9/10/2015.
  */
 public class Percolation {
+
+    private final int size;
+    UnionFind unionFind;
+    boolean[] isOpen;
+    Random random;
+
+    public Percolation(int N) {
+        unionFind = new UnionFind(N*N+2);
+        isOpen = new boolean[N*N];
+        size = N;
+        random = new Random(N*N);
+    }
+
+    public void main(String[] args){
+
+        Percolation percolation = new Percolation(100);
+
+
+
+
+
+
+    }
+    public void randOpen(){
+        open(random.nextInt(size*size));
+    }
+
+    public void open(int i){
+        isOpen[i]= true;
+        int qSize =size*size;
+
+        if (i-1>0 && isOpen[i-1]) {
+            unionFind.union(i,i-1);
+        }
+        if (i+1 < size*size-2 && isOpen[i+1]){
+            unionFind.union(i,i+1);
+        }
+        if(i-size>0 && isOpen[i-size]){
+            unionFind.union(i,i-size);
+        }
+        if (i+size<size*size && isOpen[i+size]){
+            unionFind.union(i,i+size);
+        }
+
+        if( i>=0 & i<size ){
+            unionFind.union(i,qSize);
+        }
+        if (i >= qSize-size & i<qSize) {
+            unionFind.union(i,qSize+1);
+        }
+
+    }
+
+    public boolean isPercolation(){
+        int qSize =size*size;
+        return unionFind.conected(qSize,qSize+1);
+    }
+
+
+
+
+
 }
--- src/my/percolation/UnionFind.java
+++ src/my/percolation/UnionFind.java
@@ -4,4 +4,66 @@ package my.percolation;
  * Created by Aleksandr_Kolesov on 9/10/2015.
  */
 public class UnionFind {
+
+
+    int[] id;
+    int[] sz;
+
+
+
+    UnionFind(int N) {
+        id = new int[N];
+        sz = new int[N];
+
+        for (int i = 0; i < N; i++) {
+
+            id[i] = i;
+
+            sz[i] = 1;
+        }
+    }
+
+
+    private int root(int i)
+
+    {
+
+        while (i != id[i]){
+
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+
+    }
+
+    public int find(int i) {
+        return id[i];
+    }
+
+    public void union(int p, int q) {
+        int i = root(p);
+
+        int j = root(q);
+
+        if (i == j) return;
+
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        }
+
+        else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
+
+    public boolean conected(int p, int q) {
+        return find(p) == find(q);
+
+
+    }
+
+
 }
