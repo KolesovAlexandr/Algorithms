e946923ccc182271e922129fe53083505dac08ba
Aleksandr Kolesov <aleksandr_kolesov@epam.com>
2015-10-13 17:01:24 +0300

    finish Deque

--- src/my/queue/Deque.java
+++ src/my/queue/Deque.java
@@ -1,50 +1,161 @@
 package my.queue;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
- * Created by Саша on 26.09.2015.
+ * Created by Aleksandr_Kolesov  on 26.09.2015.
  */
 public class Deque<Item> implements Iterable<Item> {
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    private class Node {
+        Item item;
+        Node next;
+        Node previous;
+    }
+
     public Deque()                           // construct an empty deque
     {
+        size = 0;
+        first = last = null;
 
     }
+
     public boolean isEmpty()                 // is the deque empty?
     {
-
-        return false;
+        return size == 0;
     }
+
     public int size()                        // return the number of items on the deque
     {
-
-        return 0;
+        return size;
     }
+
     public void addFirst(Item item)          // add the item to the front
     {
 
+        if (item == null)
+            throw new NullPointerException();
+        Node tmp = new Node();
+        tmp.item = item;
+        tmp.previous = null;
+        tmp.next = first;
+        if (first == null) {
+            first = last = tmp;
+        } else {
+            first.previous = tmp;
+            first = tmp;
+        }
+        size++;
+
+
     }
+
     public void addLast(Item item)           // add the item to the end
     {
+        if (item == null)
+            throw new NullPointerException();
+        Node tmp = new Node();
+        tmp.item = item;
+        tmp.next = null;
+        tmp.previous = last;
+        if (last == null) {
+            last = first = tmp;
+        } else {
+            last.next = tmp;
+            last = tmp;
+        }
+        size++;
+
 
     }
+
     public Item removeFirst()                // remove and return the item from the front
     {
-
-        return null;
+        if (size == 0)
+            throw new NoSuchElementException();
+        Node tmp = first;
+        first = first.next;
+        size--;
+        return tmp.item;
     }
+
     public Item removeLast()                 // remove and return the item from the end
     {
+        if (size == 0)
+            throw new NoSuchElementException();
+        Node tmp = last;
+        last = last.previous;
+        last.next = null;
+        size--;
 
-        return null;
+        return tmp.item;
     }
+
+
     public Iterator<Item> iterator()         // return an iterator over items in order from front to end
     {
+        return new Iterator<Item>() {
+            private Node itr = first;
 
-        return null;
+            @Override
+            public boolean hasNext() {
+                return itr != null;
+            }
+
+            @Override
+            public Item next() {
+                if (size == 0) throw new NoSuchElementException();
+                Node tmp = itr;
+                itr = itr.next;
+                return tmp.item;
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
     }
+
     public static void main(String[] args)   // unit testing
     {
+        Deque<String> deque = new Deque<>();
+        deque.addFirst("QQ");
+        deque.addFirst("QQ2");
+        deque.addLast("QQ3");
+        deque.addLast("QQ4");
+
+        
+        Iterator<String> iterator = deque.iterator();
+        for(;;){
+            String str = iterator.next();
+
 
     }
+
+//        deque.addLast(null);
+
+//        for (String item : deque) {
+//            System.out.println(item);
+//        }
+
+//        deque.removeFirst();
+//        deque.removeLast();
+
+//        System.out.println();
+//        for (String item : deque) {
+//            System.out.println(item);
+//        }
+//        deque.removeFirst();
+//        deque.removeLast();
+//        deque.removeFirst();
+
+
+    }
+
 }
--- src/my/queue/RandomizedQueue.java
+++ src/my/queue/RandomizedQueue.java
@@ -1,44 +1,112 @@
 package my.queue;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Random;
 
 /**
- * Created by Саша on 26.09.2015.
+ * Created by Aleksandr_Kolesov  on 26.09.2015.
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
+    private static final int DEFAULT_ARRAY_LENGTH = 10;
+    private int length = DEFAULT_ARRAY_LENGTH;
+    private int i;
+    Random random;
+
+    int size;
+    Object[] adresses;
+    private class Node {
+
+        Item itm;
+
+
+    }
+
+
     public RandomizedQueue()                 // construct an empty randomized queue
     {
-
+        size = 0;
+        int i = 0;
+        adresses = new Object[length];
+        random = new Random();
     }
+
     public boolean isEmpty()                 // is the queue empty?
     {
-
-        return false;
+        return size == 0;
     }
+
     public int size()                        // return the number of items on the queue
     {
-
-        return 0;
+        return size;
     }
+
     public void enqueue(Item item)           // add the item
     {
 
+        if (item==null) throw new NullPointerException();
+        adresses[size] = item;
+        if (adresses.length<++size){
+            length*=2;
+            Object[] tmp = new Object[length];
+            for(int j =0;j<i;j++){
+                if (adresses[j]!=null){
+
+
+                }
+            }
+        }
     }
+
     public Item dequeue()                    // remove and return a random item
     {
-
-        return null;
+        if (size==0) throw  new NoSuchElementException();
+//        int min = Math.min(size, adresses.length);
+        int index = randNotNull(adresses.length);
+        Item item = (Item) adresses[index];
+        adresses[index] = null;
+        return item;
     }
+
+    private int randNotNull(int lenth) {
+        int index = random.nextInt(lenth);
+        if (adresses[index] == null) {
+            index = randNotNull(lenth);
+        }
+        return index;
+    }
+
     public Item sample()                     // return (but do not remove) a random item
     {
-
-        return null;
+        if (size==0) throw  new NoSuchElementException();
+        int index = randNotNull(adresses.length);
+        return (Item) adresses[index];
     }
+
     public Iterator<Item> iterator()         // return an independent iterator over items in random order
     {
 
-        return null;
+
+        return new Iterator<Item>() {
+            @Override
+            public boolean hasNext() {
+                return size != 0;
+            }
+
+            @Override
+            public Item next() {
+                if (size==0) throw new NoSuchElementException();
+                return (Item) adresses[randNotNull(adresses.length)];
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+
+            }
+        };
     }
+
     public static void main(String[] args)   // unit testing
     {
 
