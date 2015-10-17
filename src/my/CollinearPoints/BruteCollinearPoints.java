ad265c349332daadadc52786a5ed8d478b8947e1
Aleksandr Kolesov <aleksandr_kolesov@epam.com>
2015-10-17 23:45:54 +0300

    collinear ponts brute finish, fast beta

--- src/my/CollinearPoints/BruteCollinearPoints.java
+++ src/my/CollinearPoints/BruteCollinearPoints.java
@@ -5,23 +5,30 @@ package my.CollinearPoints;
  */
 public class BruteCollinearPoints {
 
-    private    LineSegment[] _lineSegment;
+    private LineSegment[] _lineSegment;
     private int _countSegments = 0;
 
     public BruteCollinearPoints(Point[] points)    // finds all line segments containing 4 points
     {
+        if (points == null) throw new NullPointerException();
+
         _lineSegment = new LineSegment[points.length];
+        int i = 0;
         for (int p = 0; p < points.length; p++) {
+            if (points[p] == null) throw new NullPointerException();
             for (int q = 0; q < points.length; q++) {
                 if (p == q) continue;
+                if (points[p].compareTo(points[q]) == 0) throw new IllegalArgumentException();
+                if (points[p].compareTo(points[q]) != 1) continue;
                 for (int r = 0; r < points.length; r++) {
-                    if (q == r) continue;
+                    if (q == r || p == r) continue;
+                    if (points[q].compareTo(points[r]) != 1) continue;
+                    if (points[p].slopeTo(points[q]) != points[p].slopeTo(points[r])) continue;
                     for (int s = 0; s < points.length; s++) {
-                        if (r == s) continue;
-                        if (points[p].slopeTo(points[q]) != points[p].slopeTo(points[r])) continue;
+                        if (r == s || p == s || q == s) continue;
+                        if (points[r].compareTo(points[s]) != 1) continue;
                         if (points[p].slopeTo(points[q]) != points[p].slopeTo(points[s])) continue;
-                        _lineSegment[p]=new LineSegment(points[p],points[s]);
-                        _countSegments++;
+                        _lineSegment[_countSegments++] = new LineSegment(points[p], points[s]);
                     }
                 }
 
@@ -41,4 +48,6 @@ public class BruteCollinearPoints {
 
         return _lineSegment;
     }
+
+
 }
--- src/my/CollinearPoints/FastCollinearPoints.java
+++ src/my/CollinearPoints/FastCollinearPoints.java
@@ -1,21 +1,43 @@
 package my.CollinearPoints;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Created by Aleksandr_Kolesov on 10/16/2015.
  */
 public class FastCollinearPoints {
+    private int _countSegments = 0;
+    private LineSegment[] _lineSegment;
+
     public FastCollinearPoints(Point[] points)     // finds all line segments containing 4 or more points
     {
+        _lineSegment = new LineSegment[points.length];
+        for (int p = 0; p < points.length; p++) {
+            Arrays.sort(points, points[p].slopeOrder());
+            for (int i = 0; i < points.length - 3; i++) {
+//                if (points[i].compareTo(points[i + 1]) !=1) continue;
+//                if (points[i].compareTo(points[i + 2]) !=1) continue;
+//                if (points[i].compareTo(points[i + 3]) !=1) continue;
+                if (points[i].slopeTo(points[i + 1]) != points[i].slopeTo(points[i + 2])) continue;
+                if (points[i].slopeTo(points[i + 1]) != points[i].slopeTo(points[i + 3])) continue;
+                _lineSegment[_countSegments++] = new LineSegment(points[i], points[i + 3]);
+
+            }
+        }
+
 
     }
-    public           int numberOfSegments()        // the number of line segments
+
+    public int numberOfSegments()        // the number of line segments
     {
 
-        return 0;
+        return _countSegments;
     }
+
     public LineSegment[] segments()                // the line segments
     {
 
-        return new LineSegment[0];
+        return _lineSegment;
     }
 }
